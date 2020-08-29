3c2c2e20118ead4cb5cc63f8293911bfde7b9b4a
Taichi1854 <sherlock5128@gmail.com>
2020-08-29 20:02:48 +0900

    find_by

--- app/controllers/customer/homes_controller.rb
+++ app/controllers/customer/homes_controller.rb
@@ -2,7 +2,7 @@ class Customer::HomesController < ApplicationController
 
 
   def top
-  	@topic = Topic.find(21)
+  	@topic = Topic.find_by(title: "新型コロナウイルス等の感染予防対策について")
     @topics = Topic.all.order(created_at: :desc)
   end
 
